ebc84506e4e0da2834094cc09dfa010728175288
Dmitriy Ushakov <Ush.Dmitriy@yandex.ru>
2023-12-19 16:35:52 +0400

    feat: i18next errors ru.js

---
+++ src/locales/ru.js
@@ -0,0 +1,11 @@
+export default {
+  translation: {
+    errors: {
+      Required: 'Не должно быть пустым',
+      ParsingError: 'Ресурс не содержит валидный RSS',
+      Incorrecturl: 'Ссылка должна быть валидным URL',
+      LinkAlreadyAdded: 'RSS уже существует',
+      NetworkError: 'Ошибка сети',
+    },
+  },
+};
